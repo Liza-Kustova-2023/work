0539eeaf36bfad854d5602687798f5f715a82bf3
Liza-Kustova-2023 <126339918+Liza-Kustova-2023@users.noreply.github.com>
2023-03-16 23:04:41 +0500

    3 час
    3 час сайта

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&u#q$18ach6lt8wo7ai!i_=_doz(dj@ohqk=s+fwaf+qa6#7s)'
+SECRET_KEY = 'django-insecure-$bo(d25wn5oav%q75#!todu^%qtpj03uxmj)!iwxyd=ew@o-9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
