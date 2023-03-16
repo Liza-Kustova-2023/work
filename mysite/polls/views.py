e5f74d22fcc891303a8e17a302ec41a0c4f0ef09
Liza-Kustova-2023 <126339918+Liza-Kustova-2023@users.noreply.github.com>
2023-03-16 23:19:31 +0500

    шаблон с именем polls/index.html и передает ему контекст.
    шаблон с именем polls/index.html и передает ему контекст.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 def index(request):
@@ -13,7 +15,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
